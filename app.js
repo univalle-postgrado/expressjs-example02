b71ca6b058b01f4e985e9a23a628ab92a70348b4
Marcelo Quispe Ortega <marceloquispeortega@gmail.com>
2024-08-10 17:26:31 -0400

    Uso de identificadores autoincrementales

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 const app = express();
 const port = 3000;
 
@@ -7,7 +8,17 @@ let movies = [
     { id: 2, title: 'El Señor de los Anillos', year: 2002 }
 ];
 
+let lastId = 2;
+function generateId() {
+    lastId++;
+    return lastId;
+}
+
 app.use(express.json());
+
+const validateMovie = [
+    body('title').notEmpty().withMessage('El título es requerido'),
+];
 
 app.get('/', (req, res) => {
   res.send('Hola Mundo!');
@@ -24,22 +35,39 @@ app.get('/movies/:id', (req, res) => {
     res.status(404).json({ message: 'Película no encontrada' });
   }
 });
-app.post('/movies', (req, res) => {
-  const newMovie = req.body;
-  movies.push(newMovie);
-  res.status(201).json(newMovie);
-});
-app.put('/movies/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const movieIndex = movies.findIndex(m => m.id === id);
-  if (movieIndex !== -1) {
-    movies[movieIndex] = { ...req.body, id };
-    res.json(movies[movieIndex]);
-  } else {
-    const newMovie = { ...req.body, id };
+app.post('/movies', validateMovie, (req, res) => {
+    const errors = validationResult(req, res);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ error: errors.array() });
+    }
+
+    const newMovie = {
+        ...req.body,
+        id: generateId()
+    }
     movies.push(newMovie);
     res.status(201).json(newMovie);
-  }
+});
+app.put('/movies/:id', validateMovie, (req, res) => {
+    const errors = validationResult(req, res);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ error: errors.array() });
+    }
+        
+    const id = parseInt(req.params.id);
+    const movieIndex = movies.findIndex(m => m.id === id);
+    if (movieIndex !== -1) {
+        movies[movieIndex] = { ...req.body, id };
+        res.json(movies[movieIndex]);
+    } else {
+        const newMovie = {
+            ...req.body,
+            id: id
+        }
+        lastId = id;
+        movies.push(newMovie);
+        res.status(201).json(newMovie);
+    }
 });
 app.patch('/movies/:id', (req, res) => {
     const id = parseInt(req.params.id);
